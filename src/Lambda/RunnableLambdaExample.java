401379519726bdf5fae2d12490ec5dddf6eb562c
Vadirajp <raj.vadi5@gmail.com>
2022-11-21 22:52:12 +0530

    lambdas ex one

--- src/Lambda/RunnableLambdaExample.java
+++ src/Lambda/RunnableLambdaExample.java
@@ -1,2 +1,25 @@
-package Lambda;public class RunnableLambdaExample {
+package Lambda;
+
+public class RunnableLambdaExample {
+
+    public static void main(String[] args) {
+        /**
+         * old Java
+         */
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("inside runnable 1");
+            }
+        };
+        new Thread(runnable).start();
+
+
+        /**
+         * Java 8 approach
+         */
+        new Thread(() -> System.out.println("inside runnable 2")).start();
+
+    }
 }
